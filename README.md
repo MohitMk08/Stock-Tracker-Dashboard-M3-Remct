181f0103225e64fc83dfe9cfdc084e9cd685985b
Mohit Kuttarmare <mohitkuttarmare55@gmail.com>
2025-06-04 20:56:07 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Stock-Tracker-Dashboard-M3-Remct
---
+++ script.js
@@ -0,0 +1,72 @@
+const API_KEY = 'LE3WK1VDTRDXV1EV';
+let chart;
+
+async function loadStockData() {
+    const symbol = document.getElementById('stock-select').value;
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${API_KEY}`;
+
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        const timeSeries = data["Time Series (Daily)"];
+
+        console.log(data);
+
+        if (!timeSeries) {
+            alert("API limit reached or invalid symbol");
+            return;
+        }
+
+        const labels = Object.keys(timeSeries).reverse().slice(-30);
+        const closingPrices = labels.map(date => parseFloat(timeSeries[date]["4. close"]));
+
+        renderChart(labels, closingPrices, symbol);
+        updateTable(symbol, timeSeries[Object.keys(timeSeries)[0]]);
+    } catch (err) {
+        console.error(err);
+        alert("Failed to load data.");
+    }
+}
+
+function renderChart(labels, data, symbol) {
+    const ctx = document.getElementById('stockChart').getContext('2d');
+    if (chart) chart.destroy();
+
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels,
+            datasets: [{
+                label: `${symbol} Prices`,
+                data,
+                borderColor: 'rgba(59, 246, 84, 0.5)',
+                backgroundColor: 'rgba(59, 130, 246, 0.2)',
+                tension: .1,
+                fill: false
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: `${symbol} - Last 30 Days`
+                }
+            }
+        }
+    });
+}
+
+function updateTable(symbol, dayData) {
+    const tbody = document.getElementById('stock-info-body');
+    tbody.innerHTML = `
+    <tr>
+      <td class="p-2 border">${symbol}</td>
+      <td class="p-2 border">${parseFloat(dayData["1. open"]).toFixed(2)}</td>
+      <td class="p-2 border">${parseFloat(dayData["2. high"]).toFixed(2)}</td>
+      <td class="p-2 border">${parseFloat(dayData["3. low"]).toFixed(2)}</td>
+      <td class="p-2 border">${parseFloat(dayData["4. close"]).toFixed(2)}</td>
+      <td class="p-2 border">${parseInt(dayData["5. volume"]).toLocaleString()}</td>
+    </tr>
+  `;
+}
